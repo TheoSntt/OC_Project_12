0b6dfce54d13c5783c458621ac424a9bf9a12630
Théo <sinatti.theo@gmail.com>
2023-08-10 19:05:26 +0200

    Add default value (today) to create_date fields

--- models/client.py
+++ models/client.py
@@ -1,13 +1,14 @@
 from models.person import Person
 from sqlalchemy import Column, Integer, String, Date, ForeignKey
 from sqlalchemy.orm import relationship
+from sqlalchemy.sql import func
 
 
 class Client(Person):
     __tablename__ = 'client'
 
     enterprise_name = Column(String(255))
-    create_date = Column(Date)
+    create_date = Column(Date, server_default=func.now().date())
     last_update_date = Column(Date)
     # Relationship with Collaborator (commercial contact for the client)
     contact_id = Column(Integer, ForeignKey('collaborator.id', ondelete="SET NULL"))
--- models/contract.py
+++ models/contract.py
@@ -1,6 +1,7 @@
 from models.base import Base
 from sqlalchemy import Column, Integer, Float, String, Date, ForeignKey
 from sqlalchemy.orm import relationship
+from sqlalchemy.sql import func
 
 
 class Contract(Base):
@@ -9,7 +10,7 @@ class Contract(Base):
     id = Column(Integer, primary_key=True)
     price = Column(Float)
     remaining_to_pay = Column(Float)
-    create_date = Column(Date)
+    create_date = Column(Date, server_default=func.now().date())
     status = Column(String(255))
     # Relationship with Client (client to whom the contract is linked)
     client_id = Column(Integer, ForeignKey('client.id', ondelete="SET NULL"))
