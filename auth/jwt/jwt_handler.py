35c0eaa017b00cf11ec476530fb8f99c31ac814a
Théo <sinatti.theo@gmail.com>
2023-08-18 18:03:55 +0200

    Instantiate Controler for CRUD operation

--- auth/jwt/jwt_handler.py
+++ auth/jwt/jwt_handler.py
@@ -12,6 +12,12 @@ class JWTHandler:
         if self.secret_key is None:
             raise ValueError("JWT_SECRET_KEY environment variable is not set")
 
+    def _clear_secret_key(self):
+        if self.secret_key:
+            secret_key_ptr = ctypes.c_char_p(self.secret_key.encode("utf-8"))
+            ctypes.memset(secret_key_ptr, 0, len(self.secret_key))
+            self.secret_key = None
+
     def generate_token(self, payload):
         if self.secret_key is None:
             self._load_secret_key()
@@ -23,8 +29,21 @@ class JWTHandler:
 
         return token
 
-    def _clear_secret_key(self):
-        if self.secret_key:
-            secret_key_ptr = ctypes.c_char_p(self.secret_key.encode("utf-8"))
-            ctypes.memset(secret_key_ptr, 0, len(self.secret_key))
-            self.secret_key = None
+    def verify_jwt_token(self, token):
+        if self.secret_key is None:
+            self._load_secret_key()
+        try:
+            payload = jwt.decode(token, self.secret_key, algorithms=["HS256"])
+            # Securely clear the secret key from memory
+            self._clear_secret_key()
+            return payload
+        except jwt.ExpiredSignatureError:
+            # Securely clear the secret key from memory
+            self._clear_secret_key()
+            # Handle token expiration
+            return None
+        except jwt.DecodeError:
+            # Securely clear the secret key from memory
+            self._clear_secret_key()
+            # Handle token verification failure
+            return None
