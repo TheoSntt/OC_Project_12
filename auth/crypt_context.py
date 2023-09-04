1e01243b7919a0f6e00b5dd9a90eae487d7b9d51
Théo <sinatti.theo@gmail.com>
2023-09-04 17:36:17 +0200

    Remove test lines

--- auth/crypt_context.py
+++ auth/crypt_context.py
@@ -1,17 +1,17 @@
 from passlib.context import CryptContext
-from passlib.utils import saslprep
-import time
+# from passlib.utils import saslprep
+# import time
 
 
 cryptcontext = CryptContext.from_path('auth/security.ini')
 
 
-if __name__ == '__main__':
-    start_time = time.time()
-    top_secret = cryptcontext.hash(saslprep("password"))
-    print(top_secret)
-    print("Temps d'exécution : {:.2f}s".format(time.time() - start_time))
-    print(cryptcontext.identify(top_secret))
-    print(cryptcontext.verify("password", top_secret))
-    cfg = cryptcontext.to_string()
-    print(cfg)
+# if __name__ == '__main__':
+#     start_time = time.time()
+#     top_secret = cryptcontext.hash(saslprep("password"))
+#     print(top_secret)
+#     print("Temps d'exécution : {:.2f}s".format(time.time() - start_time))
+#     print(cryptcontext.identify(top_secret))
+#     print(cryptcontext.verify("password", top_secret))
+#     cfg = cryptcontext.to_string()
+#     print(cfg)
